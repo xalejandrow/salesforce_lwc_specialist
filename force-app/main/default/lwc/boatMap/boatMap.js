4cfae6d1f96b28e587a8b558c1c038a01121ac60
Alew <xalejandrow@gmail.com>
2022-06-16 10:44:37 -0300

    Challenge 8 - Buils the component boatsNearMe

--- force-app/main/default/lwc/boatMap/boatMap.js
+++ force-app/main/default/lwc/boatMap/boatMap.js
@@ -1,8 +1,17 @@
 // import BOATMC from the message channel
+import { LightningElement, api, wire } from "lwc";
+import { getRecord } from "lightning/uiRecordApi";
+import BOATMC from '@salesforce/messageChannel/BoatMessageChannel__c';
+import { subscribe, APPLICATION_SCOPE, MessageContext } from "lightning/messageService";
+
 
 // Declare the const LONGITUDE_FIELD for the boat's Longitude__s
 // Declare the const LATITUDE_FIELD for the boat's Latitude
 // Declare the const BOAT_FIELDS as a list of [LONGITUDE_FIELD, LATITUDE_FIELD];
+const LONGITUDE_FIELD = "Boat__c.Geolocation__Longitude__s";
+const LATITUDE_FIELD = "Boat__c.Geolocation__Latitude__s";
+const BOAT_FIELDS = [LONGITUDE_FIELD, LATITUDE_FIELD];
+
 export default class BoatMap extends LightningElement {
   // private
   subscription = null;
@@ -10,6 +19,7 @@ export default class BoatMap extends LightningElement {
 
   // Getter and Setter to allow for logic to run on recordId change
   // this getter must be public
+  @api
   get recordId() {
     return this.boatId;
   }
@@ -22,9 +32,12 @@ export default class BoatMap extends LightningElement {
   mapMarkers = [];
 
   // Initialize messageContext for Message Service
+  @wire(MessageContext)
+  messageContext;
 
   // Getting record's location to construct map markers using recordId
   // Wire the getRecord method using ('$boatId')
+  @wire(getRecord, {recordId: '$boatId', fields: BOAT_FIELDS})
   wiredRecord({ error, data }) {
     // Error handling
     if (data) {
@@ -43,10 +56,16 @@ export default class BoatMap extends LightningElement {
   subscribeMC() {
     // recordId is populated on Record Pages, and this component
     // should not update when this component is on a record page.
-    if (this.subscription || this.recordId) {
-      return;
-    }
-    // Subscribe to the message channel to retrieve the recordId and explicitly assign it to boatId.
+    if(this.subscription) { return; }
+    // Subscribe to the message channel to retrieve the recordID and assign it to boatId.
+    this.subscription = subscribe(
+        this.messageContext, 
+        BOATMC, 
+        (message) => {
+            this.boatId = message.recordId;
+        }, 
+        { scope: APPLICATION_SCOPE }
+    );
   }
 
   // Calls subscribeMC()
@@ -55,7 +74,9 @@ export default class BoatMap extends LightningElement {
   }
 
   // Creates the map markers array with the current boat's location for the map.
-  updateMap(Longitude, Latitude) {}
+  updateMap(Longitude, Latitude) {
+    this.mapMarkers = [Longitude,Latitude];
+  }
 
   // Getter method for displaying the map component, or a helper method.
   get showMap() {
---
+++ force-app/main/default/lwc/boatSearch/boatSearch.js
@@ -0,0 +1,19 @@
+import { LightningElement } from 'lwc';
+
+ // imports
+ export default class BoatSearch extends LightningElement {
+    isLoading = false;
+    
+    // Handles loading event
+    handleLoading() { }
+    
+    // Handles done loading event
+    handleDoneLoading() { }
+    
+    // Handles search boat event
+    // This custom event comes from the form
+    searchBoats(event) { }
+    
+    createNewBoat() { }
+  }
+  
